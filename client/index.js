891578c87c68caae55e65eb33958600dc49c1610
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2018-12-10 23:04:02 -0800

    cleared directory in master branch to start branches from scratch

--- client/index.js
+++
@@ -1,9 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const App = require('./components/App.js');
-const crud = require('./lib/crud.js');
-
-ReactDOM.render(
-  <App crud={crud} />,
-  document.getElementById('app')
-);
